30299860acfcadd6c7238ee6b69d280f7de0c05c
Jay Wang <jayduke15@gmail.com>
2014-01-06 17:04:32 -0800

    Initial Implementation for Shake Feature
    This feature allows users to toggle altCntrl by shaking the device.
    Still needs recalibration and refinement.
    Would like to have a pop up dialog to alert user when altCntrl toggled.

--- src/altCntrl/altCntrlActivity.java
+++ src/altCntrl/altCntrlActivity.java
@@ -8,26 +8,33 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public abstract class altCntrlActivity extends Activity implements
 		SensorEventListener {
 
-	private double[] THRESHOLDS = {30.0, -30.0};
+	private double[] THRESHOLDS = { 30.0, -30.0 };
 	private double NOISE = 5.0;
 	public SensorManager sManager;
 	private boolean on = false;
 	private Method[] methods;
 	private Object[] objects;
 	private View view;
-	public double pitch;
-	
+	public double pitch = 0.0, roll = 0.0;
+	private static boolean altCntrl = false;
+	private float mAccel; // acceleration apart from gravity
+	private float mAccelCurrent; // current acceleration including gravity
+	private float mAccelLast; // last acceleration including gravity
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		pitch = 0.0;
+		mAccel = 0.00f;
+		mAccelCurrent = SensorManager.GRAVITY_EARTH;
+		mAccelLast = SensorManager.GRAVITY_EARTH;
 	}
-	
+
 	public void altCntrlSetUp(Method[] methods, Object[] objects, View view) {
 		this.methods = methods;
 		this.objects = objects;
@@ -41,7 +48,7 @@ public abstract class altCntrlActivity extends Activity implements
 				sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
 				SensorManager.SENSOR_DELAY_FASTEST);
 	}
-	
+
 	public void onPause() {
 		super.onPause();
 		sManager.unregisterListener(this);
@@ -53,17 +60,35 @@ public abstract class altCntrlActivity extends Activity implements
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-				
+
 		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
 			return;
 		}
-		
+
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+		mAccelLast = mAccelCurrent;
+		mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+		float delta = mAccelCurrent - mAccelLast;
+		mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+
+		roll = event.values[0];
 		pitch = event.values[1];
-	
+
+		
+		if (Math.abs(mAccel) > 300.0) {
+			Log.i("Broke through: ", "" + mAccel);
+			altCntrl = true;
+		}
+
+		if (!altCntrl)
+			return;
+
 		if (Math.abs(event.values[1]) < NOISE) {
 			on = true;
 		}
-					
+
 		if (pitch > THRESHOLDS[0]) {
 			event.values[1] = 0;
 			performAction(methods[0], objects[0]);
@@ -73,11 +98,12 @@ public abstract class altCntrlActivity extends Activity implements
 		}
 
 	}
-	
+
 	public void performAction(Method action, Object object) {
-		
-		if (action == null) return;
-		
+
+		if (action == null)
+			return;
+
 		if (on) {
 			try {
 				action.invoke(object, view);
